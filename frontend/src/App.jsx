f0a20ae3b7de5b985d798309c5acbf63dda28e09
fjsi <sifangjian@qq.com>
2026-04-29 13:45:11 +0800

    修复React导入缺失问题

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SideNav from './components/layout/SideNav'
 import ToastContainer from './components/ui/Toast/ToastContainer'
